6f48e30524e47bf43f5742b7f5b94c44037cf182
Zhonx <patrick.reybaud>
2025-01-21 00:51:59 +0100

    First bootloader version

--- CM7/Core/Src/stm32h7xx_it.c
+++ CM7/Core/Src/stm32h7xx_it.c
@@ -58,6 +58,8 @@
 
 /* External variables --------------------------------------------------------*/
 extern QSPI_HandleTypeDef hqspi;
+extern TIM_HandleTypeDef htim2;
+
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -208,7 +210,7 @@ void TIM2_IRQHandler(void)
   /* USER CODE BEGIN TIM2_IRQn 0 */
 
   /* USER CODE END TIM2_IRQn 0 */
-
+  HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
 
   /* USER CODE END TIM2_IRQn 1 */
